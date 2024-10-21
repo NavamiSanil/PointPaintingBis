f0d8e129fafca03db7998f079bb069a015fab5f5
Navami S <navamisunil174@gmail.com>
2024-10-21 16:07:29 +0530

    Update test.py

--- BiSeNetv2/test.py
+++ BiSeNetv2/test.py
@@ -1,12 +1,9 @@
-"""
-
-"""
 from PIL import Image
 import argparse
 import torch
-import torchvision.transforms.transforms as transforms
+import torchvision.transforms as transforms
 import numpy as np 
-import cv2
+import cv2  
 import torch.nn.functional as F
 
 from dataset import KittiSemanticDataset, cityscapes_dataset
@@ -15,7 +12,6 @@ from utils.utils import preprocessing_cityscapes, preprocessing_kitti, postproce
 
 from model.BiseNetv2 import BiSeNetV2
 from model.ohem_loss import OhemCELoss
-from config import cfg
 
 dev = "cuda" if torch.cuda.is_available() else "cpu"
 device = torch.device(dev)
@@ -28,54 +24,40 @@ def test(args):
 
     visualizer = KittiVisualizer()
     
-    # define model
+    # Define model
     model = BiSeNetV2(19)
-    checkpoint = torch.load(args.weight_path, map_location=dev)
+    checkpoint = torch.load(args.weight_path, map_location=dev, weights_only=True)  # Suppress FutureWarning
     model.load_state_dict(checkpoint['bisenetv2'], strict=False)
     model.eval()
     model.to(device)
     Loss = OhemCELoss(0.7)
 
-
     for i in range(len(dataset)):
         image, semantic = dataset[i]
-        # cv2.imshow('s', semantic)
-        # cv2.waitKey(0)
-
         original = np.asarray(image.copy())
 
-        # (1, 3, 512, 1024)
-        if args.dataset == 'kitti':
-            image = preprocessing_kitti(image)
-        else:
-            image = preprocessing_cityscapes(image)
+        # Preprocess the image based on the dataset type
+        image = preprocessing_kitti(image) if args.dataset == 'kitti' else preprocessing_cityscapes(image)
 
-        # print(image)
-        pred = model(image) # (19, 1024, 2048)
+        # Add batch dimension and move to device
+        image = image.unsqueeze(0).to(device)  # (1, 3, H, W)
 
-        # loss from just logits (not including aux)
-        loss = Loss(pred, torch.from_numpy(semantic).unsqueeze(0))
-        # print('loss',loss.item())
+        # Get model predictions
+        with torch.no_grad():
+            pred = model(image)  # (1, 19, H, W)
+        
+        # Calculate loss (make sure semantic is on the same device as pred)
+        semantic_tensor = torch.from_numpy(semantic).unsqueeze(0).to(device)  # Move to the same device
+        loss = Loss(pred, semantic_tensor)
+        print(f'Loss for sample {i}: {loss.item()}')
 
-        pred = postprocessing(pred) # (1024, 2048) 
+        pred = postprocessing(pred)  # (H, W) 
+        pred = visualizer.semantic_to_color(pred)  # (H, W, 3) - color mapping
 
-        # coloring
-        # pred = np.stack([pred,pred,pred], axis=2).astype(np.uint8) # gray semantic
-        pred = visualizer.semantic_to_color(pred) # (1024, 2048, 3)
-
-        # get numpy image back
-        image = image.squeeze().detach().numpy().transpose(1,2,0)
-
-        # save
-        # semantic = np.stack([semantic,semantic,semantic], axis=2)
-        semantic = visualizer.semantic_to_color(semantic) # (1024, 2048, 3)
-
-        # pred_color = visualizer.add_semantic_to_image(original, pred)
+        # Save and visualize
         visualizer.visualize_test(original, pred, semantic)
-        # cv2.imshow('image', image)
-        # cv2.imshow('pred', pred)
-        # if cv2.waitKey(0) == 27:
-        #     break
+        
+        # Optional: handle exit on key press
         if visualizer.pressed_btn == 27:
             cv2.destroyAllWindows()
             cv2.imwrite('./res.jpg', pred)
@@ -83,7 +65,7 @@ def test(args):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--weight_path', type=str, default='checkpoints/BiseNetv2_150.pth',)
+    parser.add_argument('--weight_path', type=str, default='checkpoints/BiseNetv2_150.pth')
     parser.add_argument('--dataset', choices=['cityscapes', 'kitti'], default='kitti')
     args = parser.parse_args()
     test(args)
